07b56149f204b0b90f44efe5cb242f2921915e5e
dwminer <daltonminer@gmail.com>
2017-03-06 18:58:26 -0500

    Changed AI up

--- pacman/ai.py
+++ pacman/ai.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 class AI:
 	Up, Right, Down, Left = range(4)
@@ -11,4 +12,13 @@ class AI:
 		self.level = level
 
 	def next_move(self):
-		return random.choice((self.Up, self.Up, self.Down, self.Left, self.Left, self.Right))
+		val = int(time.time()) % 4
+		print(self.player.x)
+		if val < 1:
+			return self.Up
+		elif val < 2:
+			return self.Right
+		elif val < 3:
+			return self.Down
+		else:
+			return self.Left
